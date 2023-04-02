89dfea02aa7b0bc56af105fe9ed1fe33e4358917
Ashish <anshm9448@gmail.com>
2023-04-02 22:30:30 +0530

    magic

--- src/main/java/com/pythonbot/Learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/pythonbot/Learnspringframework/App02HelloWorldSpring.java
@@ -15,8 +15,8 @@ public class App02HelloWorldSpring {
         System.out.println( context.getBean("name"));
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
-        System.out.println(context.getBean("address2"));
+        System.out.println(context.getBean(Address.class));
         System.out.println(context.getBean("person2"));
-        System.out.println(context.getBean("person2 address"));
+
     }
 }
