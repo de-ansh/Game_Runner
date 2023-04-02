f0e830c54033a785dc5d0a24ec997e409091f98e
Ashish <anshm9448@gmail.com>
2023-04-02 22:58:42 +0530

    magic of spring

--- src/main/java/com/pythonbot/Learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/pythonbot/Learnspringframework/HelloWorldConfiguration.java
@@ -4,9 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 record  Address(String firstLine, String City){}
-record  Person(String name, int age){
+record  Person(String name, int age , Address address){
 
-};
+}
 @Configuration
 public class HelloWorldConfiguration {
     @Bean
@@ -14,20 +14,32 @@ public class HelloWorldConfiguration {
         return  "Ansh";
     }
     @Bean
+    public String name2(){
+        return  "Ashish";
+    }
+    @Bean
     public int age(){
         return  22;
     }
     @Bean
     public  Person person(){
-        return  new Person("Ravi", 20);
+        return  new Person("Ravi", 20, new Address("Kolkata", "West Bengal"));
     }
     @Bean(name= "address2")
     public Address address(){
         return new Address("Howrah,West Bengal", "Kolkata");
     }
+    @Bean(name= "address3")
+    public Address address3(){
+        return new Address("Ghoshpara", "Howrah");
+    }
     @Bean
-    public  Person person2(){
-        return  new Person("Ritwik", 21);
+    public  Person person2MethodCall(){
+        return  new Person(name(),age(), address());
+    }
+    @Bean
+    public  Person person3ParaMeters(String name2, int age, Address address3){
+        return  new Person(name2,age, address3);
     }
 
 }
