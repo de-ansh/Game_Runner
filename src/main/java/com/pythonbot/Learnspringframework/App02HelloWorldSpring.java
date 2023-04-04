345231de283d54111fd998096dc25fd2f98e144d
Ashish <anshm9448@gmail.com>
2023-04-04 23:29:27 +0530

    User Application context and understood the reason to use it

--- src/main/java/com/pythonbot/Learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/pythonbot/Learnspringframework/App02HelloWorldSpring.java
@@ -16,7 +16,16 @@ public class App02HelloWorldSpring {
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
        // System.out.println(context.getBean(Address.class));
+
+       // we will find the answer now about the error we got due to beans
         System.out.println(context.getBean("person2MethodCall"));
         System.out.println(context.getBean("person3ParaMeters"));
-    }
+        //Sping container :manages sping beans and their life cycles
+        //Sping container/sping context/ spring ios container are the same
+        //Types of String conatier:
+        //1. Bean Factory   : Basic Sping      Container
+        // 2.Application Context: Advance Container with enterprise-specific features
+        //Easy to use in web, Easy internationalization, easy integration with spring aop
+        //
+        }
 }
